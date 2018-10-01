88c0237044d56e7c17383246568d79d6921d395e
Dhanvi Desai <dhanvidesai99@gmail.com>
2018-10-01 23:06:14 +0530

    Socket Io commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
 const path = require('path');
 const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
+const bodyParser = require('body-parser');
 
 const publicPath = path.join(__dirname,'../public');
 console.log(publicPath);
 var app = express();
+var server = http.createServer(app);
 const port = process.env.PORT || 3000;
+
+var io = socketIo(server);
+
+io.on('connection',(socket)=>{
+  console.log(`Connected to client ${socket}`);
+
+  socket.emit('newMessage',{
+    from:'Dhanvi',
+    text:'Hey, Can you meet up!',
+    createdAt:new Date()
+  });
+
+  socket.on('messageFromClient',(data)=>{
+    console.log(data);
+  });
+
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+
+server.listen(port,()=>{
   console.log(`Server is up and running in ${port}`);
 });
